46e56cace23aa289ac99e51d14ad5f378c0ee071
Zeniitsu-u <n200507@rguktn.ac.in>
2024-08-14 01:20:29 +0530

    defVal

---
+++ defaultValues.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract DefVal{
+    uint public u; //default value is 0
+    int public i; //default value is 0
+    bool public b; //default value is "false"
+    address public a; //default value is 0x0000000000000000000000000000000000000000
+    bytes32 public b32;
+}
