900a70940d2a45fd81dd4a972392f5e7e4d01c4e
Zeniitsu-u <n200507@rguktn.ac.in>
2024-08-17 16:39:39 +0530

    error full

--- errors.sol
+++ errors.sol
@@ -16,6 +16,22 @@ contract Errors {
     uint public addr = 123;
 
     function testAssert() external view {
+        //assert is used when the condition should always be true
         assert(addr==123);
+    }
+
+    //this fn will return false as addr shouldn't be changed
+    function foo(uint _i) external{
+        addr+=1;
+        require(_i <= 10);
+        //updates to the state variable will fail as require fn gets false
+    }
+
+    error MyError(address caller, uint i);
+
+    function flee(uint _i) public view {
+        if(_i > 10){
+            revert MyError(msg.sender, _i);
+        }
     }
 }
